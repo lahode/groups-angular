ec2ffe006a771874a09d2d2dbfa7602fae5107ae
lahode <lahode@infomaniak.ch>
2017-11-20 22:31:05 +0100

    Initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GroupComponent } from './group/group.component';
+import { HomeComponent } from './home/home.component';
+import { NewGroupComponent } from './new-group/new-group.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
+    HomeComponent,
+    NewGroupComponent
   ],
   imports: [
     BrowserModule
