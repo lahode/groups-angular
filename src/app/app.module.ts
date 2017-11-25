bc51b713ce6fa693106a84107243bd005034a40e
lahode <lahode@infomaniak.ch>
2017-11-25 12:52:33 +0100

    Dynamic form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GroupComponent } from './group/group.component';
@@ -22,7 +22,8 @@ import { LogService } from '../services/log/log.service';
     PagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     GroupService,
--- src/app/new-group/new-group.component.ts
+++ src/app/new-group/new-group.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Group } from '../../models/group.model';
+import { User } from '../../models/user.model';
+
+export const user1 = new User('soloh', 'Han', 'Solo');
+export const options = ['Anyone can see the list of members',
+                        'Only the owner can see the list of members'];
+
 @Component({
   selector: 'app-new-group',
   templateUrl: './new-group.component.html',
@@ -7,9 +14,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewGroupComponent implements OnInit {
 
-  constructor() { }
+  group: Group = new Group('', '', 0, false, user1, '', '');
+  accessOptions: string[] = [];
+  showPopup: boolean = false;
 
   ngOnInit() {
+    this.accessOptions = options;
   }
 
+  changeUrl(event: any) {
+    this.group.url = event.target.value;
+  }
+
+  changeVisibility(visibility: boolean) {
+    this.group.visibility = visibility;
+  }
+
+  save() {
+    this.showPopup = true;
+  }
 }
