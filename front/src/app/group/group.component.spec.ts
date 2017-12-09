6e08773d8188ac2686a7ad286df4855afbef2495
lahode <lahode@infomaniak.ch>
2017-12-09 19:34:09 +0100

    A simple test unit on a group

--- front/src/app/group/group.component.spec.ts
+++ front/src/app/group/group.component.spec.ts
@@ -1,14 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+
+import { HomeComponent } from '../home/home.component';
 import { GroupComponent } from './group.component';
+import { PagerComponent } from '../pager/pager.component';
+
+import { GroupService } from '../../services/group/group.service';
+import { LogService } from '../../services/log/log.service';
 
 describe('GroupComponent', () => {
   let component: GroupComponent;
   let fixture: ComponentFixture<GroupComponent>;
-
+  let elButton: HTMLElement;
+  let elTitle: HTMLElement;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GroupComponent ]
+      declarations: [
+        HomeComponent,
+        GroupComponent,
+        PagerComponent
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            params: Observable.of({ id: 'GtAat0n1wZGF03TZ' }) 
+          }
+        },
+        GroupService,
+        LogService
+      ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            {path: '', component: HomeComponent}
+          ]
+        ),
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +50,18 @@ describe('GroupComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GroupComponent);
     component = fixture.componentInstance;
+    elButton = fixture.debugElement.query(By.css('.btn-primary')).nativeElement;
+    elTitle = fixture.debugElement.query(By.css('h2')).nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should contain a button "Back To Home"', async () => {
+    expect(elButton.textContent).toBe('Back To Home');
   });
+
+
+  it('should contain "Details on group \'IDEVELOP\'" as group title ', async () => {
+    expect(elTitle.textContent).toBe("Details on group 'IDEVELOP'");
+  });
+
 });
