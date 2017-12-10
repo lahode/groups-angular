83a9c26ff7b464a834339781a2b820bef3a338a4
lahode <lahode@infomaniak.ch>
2017-12-10 17:50:56 +0100

    Adding PWA cache and manifest.json to application

--- front/src/main.ts
+++ front/src/main.ts
@@ -9,4 +9,15 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(_=> {
+    let cond = 'serviceWorker' in navigator;
+    if (cond) {
+      navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('Service worker installed'))
+      .catch(err => console.error);
+    }
+    else {
+      console.log('Service worker not supported')
+    }
+  })
   .catch(err => console.log(err));
---
+++ front/src/sw.js
@@ -0,0 +1,73 @@
+// SW Version
+const SW_VERSION = '0.0.1';
+
+// Application assets
+const AppAssets = [
+    '/',
+    '/index.html',
+    '/group.module.chunk.js',
+    '/inline.bundle.js',
+    '/main.bundle.js',
+    '/polyfills.bundle.js',
+    '/styles.bundle.js',
+    '/vendor.bundle.js'
+];
+
+// Installing SW
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('app-cache-' + SW_VERSION).then(cache => cache.addAll(AppAssets))
+    ); 
+});
+
+// Activating SW
+self.addEventListener('activate', event => {
+    // Clean static cache
+    let cleanCache = caches.keys().then(ck => {
+        ck.forEach(key => {
+            if (key !== 'app-cache-' + SW_VERSION && key.match('app-cache-')) {
+                return caches.delete(key);
+            }
+        });
+    });
+});
+
+// Fetching SW
+self.addEventListener('fetch', e => {
+    // Static cache strategy for local resources
+    if (e.request.url.match(location.origin)) {
+        e.respondWith(scStrategy(e.request));
+    }
+
+    // Dynamic cache strategy for server resources
+    else if (e.request.url.match('http://localhost:4300') && e.request.method =='GET') {
+        e.respondWith(dcStrategy(e.request));
+    }
+});
+
+// Static cache strategy
+const scStrategy = (req, cacheName = 'app-cache-' + SW_VERSION) => {
+    return caches.match(req).then( cacheRes => {
+        if (cacheRes) return cacheRes;
+        return fetch(req).then(netRes => {
+            caches.open('app-cache-' + SW_VERSION).then(cache => cache.put(req, netRes));
+            return netRes.clone();
+        });
+    });
+};
+
+// Dynamic cache strategy
+const dcStrategy = (req) => {
+    return fetch(req).then(netRes => {
+        if (!netRes.ok) throw netRes;
+        caches.open('app-cache-' + SW_VERSION).then(cache => cache.put(req, netRes));     
+        return netRes.clone();
+    })
+    .catch(err => {
+        if (req.url.match('api/check-auth') && err.status == 403) {
+            return false;
+        } else {
+            return caches.match(req);
+        }
+    });
+};
